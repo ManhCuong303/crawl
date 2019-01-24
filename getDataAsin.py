bee3f3b4d9f75d8819e3a418945af61edbbbcff8
Manh Cuong <dammanhcuong303@gmail.com>
2019-01-24 14:08:42 +0700

    kkkk

--- getDataAsin.py
+++ getDataAsin.py
@@ -10,113 +10,32 @@ import sys
 from threading import Thread
 import threading
 import numpy as np
+import re
+
 this = sys.modules[__name__] # this is now your current namespace
 extracted_data = []
-headers_list = [
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2211.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2111.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.3211.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2221.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2212.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2213.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2214.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2215.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2216.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2217.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2218.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2219.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2231.90 Safari/537.36'},
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.1241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.3241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.4241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.5241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.6241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.7241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.8241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9241.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9141.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9341.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9441.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9541.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9641.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9741.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9841.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9941.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9911.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9921.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9931.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9961.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9951.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9971.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9981.90 Safari/537.36'},{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9991.90 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9992.91 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9993.92 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9994.93 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9995.94 Safari/537.36'},
-{
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.9996.95 Safari/537.36'},
+fail_getByAsin = []
 
-    ]
+json_data = open('Chrome-user-agents.json').read()
+headers_list = json.loads(json_data)
+ip_data = open('ip_list.json').read()
+ip_list = json.loads(ip_data)
 
 
 def ReadAsin(dataAsins):
     tim = 0
     headers = random.choice(headers_list)
+    proxy = random.choice(ip_list)
     for listAsin in dataAsins:
         x = listAsin['dataAsin']
         for i in x :
             url = "http://www.amazon.com/dp/" + i
             try:
-                page = requests.get(url,headers=headers, timeout=123)
+                page = requests.get(url, headers=headers, proxies=proxy, timeout=30)
                 try:
                     doc = html.fromstring(page.content)
 
-                    XPATH_NAME = '//*[@id="detailBullets_feature_div"]/ul/li[1]/span/span[1]/text()'
+                    XPATH_NAME = '//*[@id="detailBullets_feature_div"]/ul/li'
                     # XPATH_SALE_PRICE = '//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()'
                     # XPATH_ORIGINAL_PRICE = '//td[contains(text(),"List Price") or contains(text(),"M.R.P") or contains(text(),"Price")]/following-sibling::td/text()'
                     # XPATH_CATEGORY = '//a[@class="a-link-normal a-color-tertiary"]//text()'
@@ -140,6 +59,17 @@ def ReadAsin(dataAsins):
                     if page.status_code != 200:
                         print 'URL', url
                         raise ValueError('captha')
+                    RAW_NAME_cache =''
+                    for article in RAW_NAME:
+                        RAW_NAME_cache = RAW_NAME_cache + etree.tostring(article, pretty_print=True)
+
+                    RAW_NAME = re.sub('<[^<]+?>', '', RAW_NAME_cache)
+
+                    RAW_NAME = RAW_NAME.replace(" ", "")
+
+                    RAW_NAME = re.sub('\n+',',',RAW_NAME)
+
+
                     data = {
                         'NAME': RAW_NAME,
                         'head': headers,
@@ -148,44 +78,79 @@ def ReadAsin(dataAsins):
                     }
                     extracted_data.append(data)
                     # for article in RAW_NAME:
-                    print '===done===', tim, i ,RAW_NAME
+                    print '===done===',i,RAW_NAME
                     tim += 1
                 except Exception as e:
                     print 'xx',e,i
+                    kap = {
+                        'head': headers,
+                        'proxy': proxy,
+                        'url': url,
+                        'status': e,
+                    }
+                    fail_getByAsin.append(kap)
+
             except requests.exceptions.RequestException as e:  # This is the correct syntax
-                print 'yy',e
+                print 'yy',headers,url,proxy,e
+                kap = {
+                    'proxy': proxy,
+                    'url': url,
+                    'status': "Cannot connect to proxy.",
+                }
+                fail_getByAsin.append(kap)
 
 
 
 
 if __name__ == "__main__":
-    result = json.load(urllib.urlopen('data.json'))
-    AsinList = np.array_split(result, 5)
+    result = json.load(urllib.urlopen('dataTest.json'))
+    AsinList = np.array_split(result, 10)
     try:
         t1 = threading.Thread(name='t1',target=ReadAsin, args=(AsinList[0],))
         t2 = threading.Thread(name='t2',target=ReadAsin, args=(AsinList[1],))
         t3 = threading.Thread(name='t3',target=ReadAsin, args=(AsinList[2],))
         t4 = threading.Thread(name='t4',target=ReadAsin, args=(AsinList[3],))
         t5 = threading.Thread(name='t5',target=ReadAsin, args=(AsinList[4],))
+        t6 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[5],))
+        t7 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[6],))
+        t8 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[7],))
+        t9 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[8],))
+        t10 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[9],))
 
         t1.start()
         t2.start()
         t3.start()
         t4.start()
         t5.start()
+        t6.start()
+        t7.start()
+        t8.start()
+        t9.start()
+        t10.start()
 
         t1.join()
         t2.join()
         t3.join()
         t4.join()
         t5.join()
+        t6.join()
+        t7.join()
+        t8.join()
+        t9.join()
+        t10.join()
 
-        f = open('dataTest.json', 'w')
+        f = open('dataAsin.json', 'w')
         json.dump(extracted_data, f, indent=4)
         f.close()
+
+        f = open('fail_getByAsin.json', 'w')
+        json.dump(fail_getByAsin, f, indent=4)
+        f.close()
+
+
         print 'DONE !'
-    except:
-        print ("error")
+    except requests.exceptions.RequestException as e:
+        print ("error",e)
 
 
 
