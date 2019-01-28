e4667dcb08704188654c3d9dfdc00826311f960f
Manh Cuong <dammanhcuong303@gmail.com>
2019-01-28 15:48:45 +0700

    kkkk

--- getDataAsin.py
+++ getDataAsin.py
@@ -11,10 +11,12 @@ from threading import Thread
 import threading
 import numpy as np
 import re
+import multiprocessing.dummy as mp
 
 this = sys.modules[__name__] # this is now your current namespace
 extracted_data = []
 fail_getByAsin = []
+count_asin_re = []
 textC = 'ShippingWeight:,4.8'
 
 json_data = open('Chrome-user-agents.json').read()
@@ -22,13 +24,12 @@ headers_list = json.loads(json_data)
 ip_data = open('ip_list.json').read()
 ip_list = json.loads(ip_data)
 
-
 def ReadAsin(dataAsins):
 
     headers = random.choice(headers_list)
     proxy = random.choice(ip_list)
     for listAsin in dataAsins:
-        x = listAsin['dataAsin']
+        x = listAsin
         for i in x :
             reGetTitle(i)
 
@@ -47,12 +48,7 @@ def reGetTitle(i):
 
             XPATH_CHECK = '//*[@id="detailBullets_feature_div"]'
 
-
-
-
             RAW_CHECK = doc.xpath(XPATH_CHECK)
-
-
 
             RAW_CHECK_cache = ''
 
@@ -68,11 +64,10 @@ def reGetTitle(i):
             RAW_CHECK = RAW_CHECK.replace(" ", "")
             RAW_CHECK = re.sub('\n+', ',', RAW_CHECK)
 
-            if RAW_CHECK.find('ShippingWeight') != -1:
+            if RAW_CHECK.find(textC) != -1:
                 XPATH_PRICE = '//*[@id="priceblock_ourprice"]/text()'
                 XPATH_BRAN = '//*[@id="bylineInfo"]/text()'
                 XPATH_NAME = '//h1[@id="title"]//text()'
-                XPATH_PRICE = '//*[@id="priceblock_ourprice"]'
                 XPATH_RANK = '//*[@id="SalesRank"]/text()'
 
                 RAW_PRICE = doc.xpath(XPATH_PRICE)
@@ -83,22 +78,25 @@ def reGetTitle(i):
                 RAW_RANK = RAW_RANK[0]
                 RAW_BRAN = doc.xpath(XPATH_BRAN)
 
-                if len(RAW_PRICE) == 0:
-                    RAW_PRICE = 'no'
+                if len(RAW_BRAN) == 0:
+                    RAW_BRAN = doc.xpath('//*[@id="brand"]/@href')
+                    RAW_BRAN = RAW_BRAN[0].split("/",2)
+                    RAW_BRAN[0] = RAW_BRAN[1]
 
+                if len(RAW_PRICE) == 0:
+                    RAW_PRICE = ['no']
+                print RAW_BRAN,'zzzzzzzz'
                 data = {
                     'ASIN': i,
                     'NAME': RAW_NAME,
                     'RANK': RAW_RANK,
-                    'BRAN': RAW_BRAN,
+                    'BRAN': RAW_BRAN[0],
                     'PRICE': RAW_PRICE[0]
                 }
-
-                print 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',data
-
+                # print '===done===', i
                 extracted_data.append(data)
             # for article in RAW_CHECK:
-            print '===done===',i
+
 
         except Exception as e:
             print 'xx', e, url
@@ -110,52 +108,27 @@ def reGetTitle(i):
             }
             fail_getByAsin.append(kap)
             return reGetTitle(i)
-
+        count_asin_re.append(url)
+        print "===Request=== ",len(count_asin_re)
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         return  reGetTitle(i)
 
 if __name__ == "__main__":
-    result = json.load(urllib.urlopen('dataTest.json'))
-    AsinList = np.array_split(result, 10)
+    iz = 50
+    result = json.load(urllib.urlopen('data.json'))
+    AsinList = np.array_split(result, iz)
+    threads = []
     try:
-        t1 = threading.Thread(name='t1',target=ReadAsin, args=(AsinList[0],))
-        t2 = threading.Thread(name='t2',target=ReadAsin, args=(AsinList[1],))
-        t3 = threading.Thread(name='t3',target=ReadAsin, args=(AsinList[2],))
-        t4 = threading.Thread(name='t4',target=ReadAsin, args=(AsinList[3],))
-        t5 = threading.Thread(name='t5',target=ReadAsin, args=(AsinList[4],))
-        t6 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[5],))
-        t7 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[6],))
-        t8 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[7],))
-        t9 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[8],))
-        t10 = threading.Thread(name='t5', target=ReadAsin, args=(AsinList[9],))
-
-        t1.start()
-        t2.start()
-        t3.start()
-        t4.start()
-        t5.start()
-        t6.start()
-        t7.start()
-        t8.start()
-        t9.start()
-        t10.start()
-
-        t1.join()
-        t2.join()
-        t3.join()
-        t4.join()
-        t5.join()
-        t6.join()
-        t7.join()
-        t8.join()
-        t9.join()
-        t10.join()
+        for n in range(0, iz):
+            name ='t'+ str(n)
+            threads.append(threading.Thread(name=name,target=ReadAsin, args=(AsinList[n],)))
+            threads[-1].start()  # start the thread we just created
+        for t in threads:
+            t.join()
 
         f = open('dataAsin.json', 'w')
         json.dump(extracted_data, f, indent=4)
         f.close()
-
-
 
         print 'DONE !'
     except requests.exceptions.RequestException as e:
---
+++ transIP.py
@@ -0,0 +1,21 @@
+import json
+import urllib
+import numpy as np
+import re
+
+ipdata = []
+def aki():
+    f = open('Chrome-user-agents.txt', 'r')
+    data = f.readlines()
+    for line in data:
+        xx =  "https://" + line
+        xx = re.sub('\n+', '', xx)
+        print xx
+        ipdata.append({"https":xx})
+    fa = open('ip_list.json', 'w')
+    json.dump(ipdata, fa, indent=4)
+    fa.close()
+    f.close()
+
+
+aki()
